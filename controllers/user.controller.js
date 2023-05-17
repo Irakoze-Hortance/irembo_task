c6a8a6422a4fc63960693666d5a42afa2340bd68
Irakoze-Hortance <hortanceirakoze@gmail.com>
2023-05-17 10:46:19 +0200

    user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,42 +1,72 @@
-const multer= require('multer')
+const User = require('../models/user.model');
+var jwt = require('jsonwebtoken')
+const bcryptjs = require('bcryptjs');
+const config = require('../config/auth.config')
+const RevokedToken = require("../models/revokedToken.model")
 
-const User = require('../models/user.model')
+const createUser = async (req, res) => {
+  const { names, password, confirmPassword, ...userData } = req.body;
 
-var storage = multer.diskStorage({   
-    destination: function(req, file, cb) { 
-       cb(null, './uploads');    
-    }, 
-    filename: function (req, file, cb) { 
-       cb(null , file.originalname);   
+    if (!req.body) {
+    return res.status(400).json({ success: false, message: 'Bad Request' });
     }
- });
+    console.log(req.body)
 
- const docFilter=(req,file,cb)=>{
-    if(!file.originalname.match(/\.(jpeg|jpg|png)$/)){
-        return cb(new Error('You can only upload picture file!'),false);
+    if(password !== confirmPassword) {
+        console.log("Passwords do not match")
     }
-    cb(null,true);
-}
 
-const upload = multer({ storage: storage })
+  try {
+    const user = new User(req.body);
+    const salt = await bcryptjs.genSalt(8)
+    user.password = await bcryptjs.hash(password,salt);
+    user.profilePhoto= req.files;
+    const userData = await user.save();
 
-createUser= async(req,res)=>{
-    let body=req.body
-    if(!body){
-        return res.status(400).json({
-            success:false,
-            error:'You must provide user details',
-        })
+    res.status(200).json({ success: true, data: userData});
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { names, password } = req.body;
+
+    const user = await User.findOne({ names });
+
+    if (!user || !(await bcryptjs.compare(password, user.password))) {
+      return res.status(401).json({ error: 'Invalid names or password' });
     }
-    body = {...body,profilePhoto :req.files}
-    
-    const user=new User(body)
 
-   await user.save()
-            return res.status(201).json({
-                success:true,
-                user:user,
-                message:'New user added ...'
-            })
+    const token = jwt.sign({userId:user._id},config.secret,{
+        expiresIn:'8h'
+    })
+    res.json({token,user});
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-}
+const logoutUser = async (req, res) => {
+    try {
+      // Assuming you have a middleware that verifies and decodes the token from the request headers
+      const token = req.headers.authorization.split(' ')[1];
+
+      const revokedToken = new RevokedToken({ token });
+      await revokedToken.save();
+  
+      res.json({ message: 'Logout successful' });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+module.exports = {
+  createUser,
+  loginUser,
+  logoutUser,
+};
+
+
